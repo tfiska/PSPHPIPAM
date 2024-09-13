8eaf8fc4ae454ee1341b3719186bdeef4bb7fcc5
Trygve FiskåRamfjord <trygve.fiskaramfjord@tietoevry.com>
2024-09-13 13:43:05 +0200

    added parameter force_section_no_strictMode

--- PSPHPIPAM/Functions/Public/Subnets/New-PhpIpamSubnet.ps1
+++ PSPHPIPAM/Functions/Public/Subnets/New-PhpIpamSubnet.ps1
@@ -20,7 +20,8 @@ function New-PhpIpamSubnet {
         [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
         [validateScript( { $_ -is [system.collections.hashtable] })]
         $Params,
-        [parameter(mandatory = $false)][hashtable]$PhpIpamSession=@{}
+        [parameter(mandatory = $false)][hashtable]$PhpIpamSession=@{},
+        [parameter(mandatory = $false)][switch]$force_section_no_strictMode
     )
     begin {
 
@@ -29,7 +30,13 @@ function New-PhpIpamSubnet {
         #$Params.masterSubnetId
         
         $r = Invoke-PhpIpamExecute -method post -ContentType "application/json" -controller subnets -params $Params -PhpIpamSession $PhpIpamSession
-
+        if($r -and $r.message -match "^Subnet overlaps with " -and $force_section_no_strictMode) {
+            $OldSection=Get-PhpIpamSectionByID -PhpIpamSession  $PhpIpamSession -id $Params.sectionId
+            $Section=Update-PhpIpamSection -PhpIpamSession  $PhpIpamSession -Params @{id=$Params.sectionId;strictMode=0}
+            $r = Invoke-PhpIpamExecute -method post -ContentType "application/json" -controller subnets -params $Params -PhpIpamSession $PhpIpamSession
+            $Section=Update-PhpIpamSection -PhpIpamSession  $PhpIpamSession -Params @{id=$Section.id;strictMode=$OldSection.strictMode}
+            
+        }
         if ($r -and $r.success) {
             if ($r.id) {
                 $ReturnData=Get-PhpIpamSubnetByID -id $r.id  -PhpIpamSession $PhpIpamSession
